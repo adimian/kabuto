facdd6071e41179e39e3b3441d75702284a698f3
Maarten De Paepe <maarten@adimian.com>
2015-06-05 14:32:13 +0200

    running rabbit mq related stuff as sudo

--- runtests.sh
+++ runtests.sh
@@ -3,8 +3,8 @@ export PYTHONPATH=kabuto
 virtualenv -p python3 .ve
 source .ve/bin/activate
 pip install -r requirements.txt
-service rabbitmq-server start
-rabbitmqctl add_user kabuto kabuto
-rabbitmqctl add_vhost celery
-rabbitmqctl set_permissions -p celery kabuto ".*" ".*" ".*"
+sudo service rabbitmq-server start
+sudo rabbitmqctl add_user kabuto kabuto
+sudo rabbitmqctl add_vhost celery
+sudo rabbitmqctl set_permissions -p celery kabuto ".*" ".*" ".*"
 find . -name "*.pyc" -delete && py.test $@
