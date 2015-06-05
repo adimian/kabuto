08709a763a6b4da9772b14ebbb287a3baa7a3068
Maarten De Paepe <maarten@adimian.com>
2015-06-05 12:56:52 +0200

    added rabbitmq init scripts for celery

--- runtests.sh
+++ runtests.sh
@@ -3,4 +3,8 @@ export PYTHONPATH=kabuto
 virtualenv -p python3 .ve
 source .ve/bin/activate
 pip install -r requirements.txt
+service rabbitmq-server start
+rabbitmqctl add_user kabuto kabuto
+rabbitmqctl add_vhost celery
+rabbitmqctl set_permissions -p celery kabuto ".*" ".*" ".*"
 find . -name "*.pyc" -delete && py.test $@
