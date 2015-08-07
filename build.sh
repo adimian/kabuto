eab7fdd5c93d80dbe79645671c2f433620673fc7
Maarten De Paepe <maarten@adimian.com>
2015-08-07 14:44:45 +0200

    fixed build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running build for kabuto"
-docker build -t registry.adimian.com/kabuto/kabuto
+docker build -t registry.adimian.com/kabuto/kabuto .
 docker push registry.adimian.com/kabuto/kabuto
