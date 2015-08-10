f2f6b3432373b4118df1bdd76c0b5be438be3be1
Maarten De Paepe <maarten@adimian.com>
2015-08-10 10:46:02 +0200

    added anonymous attr to user for raven

--- kabuto/api.py
+++ kabuto/api.py
@@ -73,6 +73,9 @@ class User(db.Model):
     def is_authenticated(self):
         return True
 
+    def is_anonymous(self):
+        return False
+
     def get_id(self):
         return self.login
 
